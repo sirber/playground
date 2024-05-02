241e7ae1de3ca234e57017f9231e46faaaca5902
Stephane Berube <sirber@protonmail.com>
2024-05-02 08:35:12 -0400

    added: COuntrepetition for C# + tests

--- README.md
+++ README.md
@@ -16,10 +16,14 @@ When possible, unit tests will be provided.
 
 ## Go
 
-- Install gi:
+https://go.dev/doc/install
+
+## dotNet
+
+https://dotnet.microsoft.com/en-us/download
 
 ## Experiments
 
-- Count Repetition (JS, PHP, Go)
+- Count Repetition (JS, PHP, C#)
 
 Count the number of repetitions. For exemple, [1, 2, 2, 3, 3, 3], we have 3 repetitions.
---
+++ dotNet/count_repetition/Program.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+public class CountRepetition
+{
+  public int GetCountRepetition(int[] source)
+  {
+    int count = 0;
+    var map = new Dictionary<int, int>();
+
+    foreach (int number in source)
+    {
+      if (map.ContainsKey(number))
+      {
+        int value = map.GetValueOrDefault(number);
+        map[number] = value + 1;
+      }
+      else
+      {
+        map.Add(number, 0);
+      }
+    }
+
+    foreach (KeyValuePair<int, int> pair in map)
+    {
+      count += pair.Value;
+    }
+
+    return count;
+  }
+}
+
+[TestFixture]
+public class CountRepetitionTests
+{
+  [Test]
+  public void GetCountRepetition_Test1()
+  {
+    // Arrange
+    var repetition = new CountRepetition();
+    var test = new int[] { 1, 2, 2, 3, 3, 3 };
+
+    // Act
+    int result = repetition.GetCountRepetition(test);
+
+    // Assert
+    Assert.That(result, Is.EqualTo(3));
+  }
+
+  [Test]
+  public void GetCountRepetition_Test2()
+  {
+    // Arrange
+    var repetition = new CountRepetition();
+    var test = new int[] { 1, 1, 2, 2, 3, 3 };
+
+    // Act
+    int result = repetition.GetCountRepetition(test);
+
+    // Assert
+    Assert.That(result, Is.EqualTo(3));
+  }
+
+  [Test]
+  public void GetCountRepetition_Test3()
+  {
+    // Arrange
+    var repetition = new CountRepetition();
+    var test = new int[] { 1, 2, 1, 2, 1, 2 };
+
+    // Act
+    int result = repetition.GetCountRepetition(test);
+
+    // Assert
+    Assert.That(result, Is.EqualTo(4));
+  }
+}
