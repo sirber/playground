343952f88fd03d02e7167ae1cfea71b9184a5b0c
Stephane Berube <sirber@protonmail.com>
2024-05-03 14:46:50 -0400

    added: not found

--- php/pure/src/public/index.php
+++ php/pure/src/public/index.php
@@ -4,11 +4,16 @@ require_once __DIR__ . '/../private/bootstrap.php';
 
 $request = $_SERVER["REQUEST_URI"];
 switch ($request) {
+  case '/':
+    phpinfo();
+    break;
+
   case '/users':
     echo "users";
     break;
 
   default:
-    phpinfo();
+    header("HTTP/1.0 404 Not Found");
+    echo "not found!";
     break;
 }
