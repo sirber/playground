f704a910b3754499246e18cc4ad4a1c6e3464478
Stephane Berube <sirber@protonmail.com>
2024-05-03 16:12:37 -0400

    added: typing for Session class

--- php/pure/src/private/Service/Session.php
+++ php/pure/src/private/Service/Session.php
@@ -4,12 +4,12 @@ namespace App\Service;
 
 class Session
 {
-  public static function get(string $key)
+  public static function get(string $key): mixed
   {
     return $_SESSION[$key];
   }
 
-  public static function set(string $key, $value)
+  public static function set(string $key, mixed $value): void
   {
     $_SESSION[$key] = $value;
   }
