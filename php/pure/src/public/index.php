12a9b319ce39eb3bd00e94d7ffa8d69b54547fec
Stephane Berube <sirber@protonmail.com>
2024-05-03 14:45:52 -0400

    added: simple router

--- php/pure/src/public/index.php
+++ php/pure/src/public/index.php
@@ -2,5 +2,13 @@
 
 require_once __DIR__ . '/../private/bootstrap.php';
 
-phpinfo();
+$request = $_SERVER["REQUEST_URI"];
+switch ($request) {
+  case '/users':
+    echo "users";
+    break;
 
+  default:
+    phpinfo();
+    break;
+}
