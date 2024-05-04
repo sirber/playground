27d5dd4c1103c3d3bafead0c72c0b4f37518d79b
Stephane Berube <sirber@protonmail.com>
2024-05-03 22:18:38 -0400

    added: connect to mysql

--- php/pure/src/private/Repository/Database.php
+++ php/pure/src/private/Repository/Database.php
@@ -4,8 +4,15 @@ namespace App\Repository;
 
 abstract class Database
 {
+  private $db;
+
   public function __construct()
   {
-    // TODO: connect PDO using ENV vars    
+    $this->db = new \PDO($_ENV['PDO_DB_STRING'], $_ENV['PDO_DB_USER'], $_ENV['PDO_DB_PASS']);
+  }
+
+  public function query(string $query)
+  {
+    return $this->db->query($query);
   }
 }
