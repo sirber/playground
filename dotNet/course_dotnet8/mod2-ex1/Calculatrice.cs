8be7e902f38c6d5a75ea5cb6d9a47bd08889c879
Stephane Berube <sirber83@gmail.com>
2024-05-14 08:45:54 -0400

    updated: mod2-ex1

--- dotNet/course_dotnet8/mod2-ex1/Calculatrice.cs
+++ dotNet/course_dotnet8/mod2-ex1/Calculatrice.cs
@@ -43,6 +43,11 @@ public class Calculatrice
   /// <returns>le résultat</returns>
   public int Division(int num1, int num2)
   {
+    if (num2 == 0)
+    {
+      return 0;
+    }
+
     return num1 / num2;
   }
 
