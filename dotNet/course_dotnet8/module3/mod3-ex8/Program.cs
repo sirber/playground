c2201ef372efcc4ae9a25b555aef3cd1c26c6bf3
Stephane Berube <sirber83@gmail.com>
2024-05-21 08:42:21 -0400

    updated: mod3-ex8

--- dotNet/course_dotnet8/module3/mod3-ex8/Program.cs
+++ dotNet/course_dotnet8/module3/mod3-ex8/Program.cs
@@ -6,11 +6,21 @@ try
 {
   throw new MonException();
 }
+catch (MonException e)
+when (e.Message is not null)
+{
+  Console.WriteLine("MonException catched!");
+  Console.WriteLine(e.Message);
+}
 catch (MonException)
 {
-  Console.WriteLine(" MonException catched!");
+  Console.WriteLine("MonException catched!");
 }
 catch
 {
-  Console.WriteLine(" Exception catched!");
+  Console.WriteLine("Exception catched!");
+}
+finally
+{
+  Console.WriteLine("Finally!");
 }
